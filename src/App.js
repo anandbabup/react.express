1af20e8c9980c5ef27f306973e6c5552ab44ffd2
anandbabup <anandbabu.x.pancharathinam@kp.org>
2023-09-03 12:15:05 +0530

    using useState

--- src/App.js
+++ src/App.js
@@ -1,61 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function Card({ loading, error, title, subtitle }) {
-  let content
-
-  if (error) {
-    content = 'Error'
-  } else if (loading) {
-    content = <h3 style={styles.empty}>Loading...</h3>
-  } else {
-    content = (
-      <div>
-        <h1 style={styles.title}>{title}</h1>
-        {subtitle ? (
-          <h2 style={styles.subtitle}>{subtitle}</h2>
-        ) : (
-          <h3 style={styles.empty}>No subtitle</h3>
-        )}
-      </div>
-    )
-  }
-
-  return <div style={styles.card}>{content}</div>
+const randomDiceRoll = () => {
+  return Math.floor(Math.random() * 6) + 1
 }
 
 function App() {
+  const [diceRolls, setDiceRolls] = useState([1, 2, 3])
+
   return (
     <div>
-      <Card error={true} />
-      <Card loading={true} />
-      <Card loading={false} title={'Title'} subtitle={'Subtitle'} />
+      <button
+        onClick={() => {
+          setDiceRolls([...diceRolls, randomDiceRoll()])
+        }}
+      >
+        Roll dice
+      </button>
+      <ul>
+        {diceRolls.map((diceRoll, index) => (
+          <li key={index}>{diceRoll}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 
-const styles = {
-  card: {
-    padding: '20px',
-    margin: '20px',
-    textAlign: 'center',
-    color: 'white',
-    backgroundColor: 'steelblue',
-    border: '1px solid rgba(0,0,0,0.15)',
-  },
-  title: {
-    fontSize: '18px',
-    lineHeight: '24px',
-  },
-  subtitle: {
-    fontSize: '14px',
-    lineHeight: '18px',
-  },
-  empty: {
-    fontSize: '12px',
-    lineHeight: '15px',
-    opacity: '0.5',
-  },
-}
 export default App;
