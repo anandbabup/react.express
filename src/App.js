e270f99981c37a8e8955ed09c26d211c5e8ae94c
anandbabup <anandbabu.x.pancharathinam@kp.org>
2023-09-03 10:54:37 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Card({ title, subtitle }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.card}>
+      <h1 style={styles.title}>{title}</h1>
+      {subtitle && <h2 style={styles.subtitle}>{subtitle}</h2>}
     </div>
-  );
+  )
 }
 
+function App() {
+  return (
+    <div>
+      <Card title={'Title'} />
+      <Card title={'Title'} subtitle={'Subtitle'} />
+    </div>
+  )
+}
+
+const styles = {
+  card: {
+    padding: '20px',
+    margin: '20px',
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: 'steelblue',
+    border: '1px solid rgba(0,0,0,0.15)',
+  },
+  title: {
+    fontSize: '18px',
+    lineHeight: '24px',
+  },
+  subtitle: {
+    fontSize: '14px',
+    lineHeight: '18px',
+  },
+}
 export default App;
