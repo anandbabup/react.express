fb11abf548fb0e9df893e99a2930a2bf054b9a0b
anandbabup <anandbabu.x.pancharathinam@kp.org>
2023-09-04 07:59:13 +0530

    useEffect with dependency

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useReducer } from 'react'
+import React, { useState, useEffect } from 'react'
 
-const types = {
-  PET: 'PET',
-  COLOR: 'COLOR',
-}
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case types.PET:
-      return { ...state, pet: action.value }
-    case types.COLOR:
-      return { ...state, color: action.value }
-  }
-}
-
-const initialState = {
-  color: 'black',
-  pet: 'cat',
-}
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [count, setCount] = useState(0)
+  const color = count % 5 === 0 ? 'red' : 'blue'
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color
+  }, [color])
 
   return (
-    <div>
-      <label>Choose a color and a pet: </label>
-      <br />
-      <select
-        value={state.color}
-        onChange={event => {
-          dispatch({ type: types.COLOR, value: event.target.value })
-        }}
-      >
-        <option value="black">Black</option>
-        <option value="pink">Pink</option>
-        <option value="blue">Blue</option>
-      </select>
-      <select
-        value={state.pet}
-        onChange={event => {
-          dispatch({ type: types.PET, value: event.target.value })
-        }}
-      >
-        <option value="cat">Cat</option>
-        <option value="dog">Dog</option>
-        <option value="mouse">Mouse</option>
-      </select>
-      <br />
-      <br />
-      You chose a {state.color} {state.pet}
-    </div>
+    <button
+      onClick={() => {
+        setCount(count + 1)
+      }}
+    >
+      Click HERE to increment: {count}
+    </button>
   )
 }
 
